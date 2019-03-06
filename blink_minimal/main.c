3af7ee2b940c00d345a812d584eb0e7598a0abcb
Tim Schumacher <tim@timakro.de>
2019-03-06 16:47:39 +0100

    Add volatile keyword

--- blink_minimal/main.c
+++ blink_minimal/main.c
@@ -3,16 +3,16 @@
 void main ()
 {
     // Enable I/O port A clock
-    *(uint32_t *)0x40021018 = 0x00000004;
+    *(volatile uint32_t *)0x40021018 = 0x00000004;
 
     // Configure pin 11 as push-pull output
-    *(uint32_t *)0x40010804 &= 0xFFFF0FFF;
-    *(uint32_t *)0x40010804 |= 0x00002000;
+    *(volatile uint32_t *)0x40010804 &= 0xFFFF0FFF;
+    *(volatile uint32_t *)0x40010804 |= 0x00002000;
 
     while (1) {
-        *(uint32_t *)0x40010810 = (1<<11); // Set pin 11
+        *(volatile uint32_t *)0x40010810 = (1<<11); // Set pin 11
         for (int i = 0; i < 1000000; i++);
-        *(uint32_t *)0x40010814 = (1<<11); // Unset pin 11
+        *(volatile uint32_t *)0x40010814 = (1<<11); // Unset pin 11
         for (int i = 0; i < 1000000; i++);
     }
 }
